fa41c0bfc71712e042eb68b39de50251e0dcb9b2
Elkeno Jones <elkeno.jones@binarystar.ca>
2024-05-26 12:29:49 -0300

    feat: update composer file and entry point

--- drupal/entrypoint.sh
+++ drupal/entrypoint.sh
@@ -6,8 +6,15 @@ composer require "drush/drush" "drupal/admin_toolbar:^3.4" "drupal/blog:^3" "dru
 if [ ! -e "/usr/bin/drush" ]; then
   ln -s /opt/drupal/vendor/bin/drush /usr/bin/drush
 fi
+
+# symlink the composer file
+rm /opt/drupal/composer.json /opt/drupal/composer.lock
+ln -s /tmp/composer/composer.json /opt/drupal/composer.json
+ln -s /tmp/composer/composer.lock /opt/drupal/composer.lock
+composer install -n
+
 # /opt/drupal/vendor/bin/drush config:import -y
 
 service apache2 start
 # the above line should keep the container running
-# tail -f /dev/null
+tail -f /dev/null
